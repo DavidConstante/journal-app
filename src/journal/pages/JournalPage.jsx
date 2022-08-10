858c3d89b61217cdf91e586469515868603e4bda
DavidConstante <omileconstante@gmail.com>
2022-08-10 12:21:22 -0500

    Create the pages for see the notes, navbar , sidebar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,33 @@
+import { IconButton } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
 import React from 'react'
+import JournalLayout from '../layout/JournalLayout'
+import { NoteView } from '../views/NoteView'
+import { NothingSelectedView } from '../views/NothingSelectedView'
 
 const JournalPage = () => {
     return (
-        <div>
-            JournalPage
-        </div>
+        <JournalLayout>
+
+            <NothingSelectedView />
+            {/* <NoteView /> */}
+
+
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.7 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }} />
+            </IconButton>
+
+        </JournalLayout>
     )
 }
 
