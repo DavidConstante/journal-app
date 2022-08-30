4ffb982b175b02b8a3ce052244156f0ad83c4659
DavidConstante <omileconstante@gmail.com>
2022-08-30 00:00:35 -0500

    Protect auth routhes when user login

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,30 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
 import AuthRoutes from '../auth/routes/AuthRoutes'
 import JournalRoutes from '../journal/routes/Journalroutes'
 
+import { CheckingAuth } from '../ui'
+import { useCheckAuth } from '../hooks'
+
 const AppRouter = () => {
+
+    const { status } = useCheckAuth();
+
+    if (status === 'checking') {
+        return <CheckingAuth />
+    }
+
     return (
         <Routes>
 
-            {/* Login y Registro */}
-            <Route path='/auth/*' element={<AuthRoutes />} />
+            {
+                (status === 'authenticated')
+                    ? < Route path='/*' element={<JournalRoutes />} /> // Journal App
+                    : <Route path='/auth/*' element={<AuthRoutes />} /> // Login and Register
+            }
 
-            {/* Journal App */}
-            <Route path='/*' element={<JournalRoutes />} />
+            <Route path='/*' element={<Navigate to='/auth/login' />} />
+
 
         </Routes>
     )
